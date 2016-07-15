175feadf524654eb102308e814be0a9ebbefe8f7
Forrest Filler <forrestfiller@users.noreply.github.com>
2016-07-15 17:32:09 -0400

    added placeholder for image to parse out

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,15 @@ var router = express.Router()
 
 var politicians = [
 	{id: 0, age: 70, firstname: 'Donald', lastname: 'Trump', party: 'r', 
-	state: 'NY', positions: ['Real Estate', 'Investor', 'Golfer']},
+	state: 'NY', positions: ['Real Estate', 'Investor', 'Golfer'], image: '0image'},
 	{id: 1, age: 68, firstname: 'Hillary', lastname: 'Clinton', party: 'd', 
-	state: 'NY', positions: ['Secretary of State','Senator', 'First Lady']},
+	state: 'NY', positions: ['Secretary of State','Senator', 'First Lady'], image: '1image'},
 	{id: 2, age: 58, firstname: 'Chris', lastname: 'Christie', party: 'r', 
-	state: 'NJ', positions: ['Governor','US Attorney for District of NJ']},
+	state: 'NJ', positions: ['Governor','US Attorney for District of NJ'], image: '2image'},
 	{id: 3, age: 54, firstname: 'Barack', lastname: 'Obama', party: 'd', 
-	state: 'IL', positions: ['President','Senator', 'Lawyer']},
+	state: 'IL', positions: ['President','Senator', 'Lawyer'], image: '3image'},
 	{id: 4, age: 53, firstname: 'Paul', lastname: 'Ryan', party: 'r', 
-	state: 'WI', positions: ['Speaker of the House','Senator']}
+	state: 'WI', positions: ['Speaker of the House','Senator'], image: '4image'}
 ]
 // add state, positions, then three more politicians
 
